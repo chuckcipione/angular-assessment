5cb2c6ce60c6e0f15ddee34a4e3bfa1f6751feb1
Chuck <chuckcipione@yahoo.com>
2017-11-02 08:48:38 -0500

    angular-assessment

---
+++ scripts/productDetailsCtrl.js
@@ -0,0 +1,11 @@
+angular.module("devApp").controller('productDetailsCtrl', function($scope, shopService, $stateParams) {
+    
+      $scope.getProductId = function(){
+          shopService.getProductId($stateParams).then(response => {
+              $scope.product = response.data;
+          });
+      }
+    
+      $scope.getProductId();
+    
+    });
